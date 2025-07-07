d3481b362ace18af8e6899f7d6722d864f743ff8
Nguyen Ngoc <knkidngoc@gmail.com>
2025-07-07 20:24:52 +0700

    add test new downloaded image

--- scripts/predict.py
+++ scripts/predict.py
@@ -2,6 +2,9 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import matplotlib.pyplot as plt
+from PIL import Image
+
 from app.model_loader import load_model
 from app.predict_image import predict_utils
 from scripts.utils import model_path
@@ -23,18 +26,18 @@ pred_class, confidence = predict_utils(image_bytes, model, class_names)
 print(f"{pred_class}: {confidence}")
 
 # test new downloaded image
-# predict_dir = "predict_image"
-# for img_name in os.listdir(predict_dir):
-#     if img_name.lower().endswith(".jpg", ".jpeg", ".png"):
-#         path = os.path.join(img_name, predict_dir)
-#         label, confidence = predict_image(path, model, class_names)
-#         print(f"{label}: {confidence}")
+predict_dir = "predict_image"
+for img_name in os.listdir(predict_dir):
+    if img_name.lower().endswith(".jpg", ".jpeg", ".png"):
+        path = os.path.join(img_name, predict_dir)
+        label, confidence = predict_utils(path, model, class_names)
+        print(f"{label}: {confidence}")
 
-#         for i, name in enumerate(class_names):
-#             print(f"{name}: {confidence[i]:.2f}")
+        for i, name in enumerate(class_names):
+            print(f"{name}: {confidence[i]:.2f}")
 
-#         image = Image.open(path).convert("RGB")
-#         plt.imshow(image)
-#         plt.title(f"{img_name}: {label}")
-#         plt.axis("off")
-#         plt.show()
+        image = Image.open(path).convert("RGB")
+        plt.imshow(image)
+        plt.title(f"{img_name}: {label}")
+        plt.axis("off")
+        plt.show()
